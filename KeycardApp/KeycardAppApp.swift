2b166e672e5baee07ca1ffdbb5c2a57541965a72
Rick <rmartinez1611@gmail.com>
2021-02-09 15:33:05 -0500

    Initial Commit

---
+++ KeycardApp/KeycardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeycardAppApp.swift
+//  KeycardApp
+//
+//  Created by Ricardo Martinez on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct KeycardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
